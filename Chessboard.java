cb89ffa9ab11dc30ac80f0ca876617e2fbf95056
markplotlib <markplotlib@gmail.com>
2020-02-18 08:40:43 -0800

    remove commented-out code; rename vars in toChess()

--- Chessboard.java
+++ Chessboard.java
@@ -15,17 +15,15 @@ public class Chessboard {
 
 	public Chessboard(int side) {
         this.side = side;
-        // history = new int[side * side];
-        // moves = 0;
         history = new LinkedHashSet<Integer>();
     }
 
     public String toChess(int m) {
         // Return algebraic chess notation for square m in an r x c chess board.
         int r = m % side + 97;
-        char ch = (char) r;
-        int c = side - (m / side);
-        return Character.toString(ch) + Integer.toString(c);
+        char row = (char) r;
+        int col = side - (m / side);
+        return Character.toString(row) + Integer.toString(col);
     }
 
     public boolean isKnightMove(int m0, int m1) {
@@ -53,17 +51,12 @@ public class Chessboard {
 
     public boolean beenHereBefore(int m) {
         // If the knight has already been in that space, then move is invalid
-        //for (int i = 0; i < history.length; i++)
-        //   if (m == history[i])
-        //        return true;
-        //return false;
         return history.contains(m);
     }
 
     public void trackMove(int m) {
         // stores history of spaces occupied by knight
         // m is integer from 0 to side * side (e.g. to 63)
-        // history[moves++] = m;
         history.add(m);
     }
 
@@ -71,17 +64,12 @@ public class Chessboard {
         // returns string of move sequence, in algebraic notation
         trackMove(m);
         StringBuilder str = new StringBuilder();
-        //for (int i = 0; i < moves; i++) {
-        //    str.append(toChess(history[i]) + " ");
-        //}
         for (int item : history) {
-           str.append(toChess(item) + " "); 
+           str.append(toChess(item) + " ");
         }
         return str.toString();
     }
 
     private int side;       // n for an n x n square chessboard
-    //private int[] history;  // integer array of move sequence
-    //private int moves;      // integer move counter
     private final LinkedHashSet<Integer> history;
 }
