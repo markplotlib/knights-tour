047e373a2c8437fbbf075683e4c00e4ae5134591
markplotlib <markplotlib@gmail.com>
2020-02-29 10:27:52 -0800

    prototype OOP Chessboard in Python

--- BruteForce.py
+++ BruteForce.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+from Chessboard import Chessboard
 # code must be done in Python (v3).
 # We'll be using r x c chess boards of various sizes with paths displayed in
 # algebraic chess notation (bottom left corner is square a1,
@@ -12,25 +14,26 @@
 # to show progress. Your output has to look substantially similar to mine,
 # as shown here for the start of running it for a 4x4 board:
 
-from decimal import Decimal
+def show_table():
+    #### HARD CODED
+    t1 = 3.52
+    time_total = 2.1E13
+    prg1 = 1E6
+    prg1_perc = 1/100000    # ask Kevin how to make this show 0.00001%
+    tour_done = False
+    #### HARD CODED
 
-#### HARD CODED
-t1 = 3.52
-time_total = 2.1E13
-prg1 = 1E6
-prg1_perc = 1/100000    # ask Kevin how to make this show 0.00001%
-tour_done = False
-#### HARD CODED
+    progress_ratio = '%.1E' % Decimal(str(prg1)) + '/' + '%.1E' % Decimal(str(time_total))
+    progress_percent = '(' + str(prg1_perc) + '%)'
+    progress_stats = " ".join([progress_ratio, progress_percent])
 
-progress_ratio = '%.1E' % Decimal(str(prg1)) + '/' + '%.1E' % Decimal(str(time_total))
-progress_percent = '(' + str(prg1_perc) + '%)'
-progress_stats = " ".join([progress_ratio, progress_percent])
+    outcome = "success" if tour_done else "failure"
 
-outcome = "success" if tour_done else "failure"
+    colnames = ["time", "*"*8 + " progress " + "*"*8, "outcome"]
+    data = [str(t1),progress_stats,outcome]
 
-colnames = ["time", "*"*8 + " progress " + "*"*8, "outcome"]
-data = [str(t1),progress_stats,outcome]
+    return " ".join(colnames) + "\n" + " ".join(data)
+
 
 if __name__ == '__main__':
-    print(" ".join(colnames))
-    print(" ".join(data))
+    print(show_table())
---
+++ Chessboard.py
@@ -0,0 +1,4 @@
+class Chessboard:
+    def __init__(self, side):
+        self.side = side
+        history = {}
