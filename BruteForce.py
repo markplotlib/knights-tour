731ccf2b4c0f8b40ffe4ca2b3822da73386a8f85
markplotlib <markplotlib@gmail.com>
2020-03-18 12:55:16 -0700

    successfully revert back to multi-file structure. BruteForce to be kept in separate file, due to factorial runtime

--- BruteForce.py
+++ BruteForce.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from Chessboard import *
 from itertools import permutations
 # code must be done in Python (v3).
 # We'll be using r x c chess boards of various sizes with paths displayed in
@@ -13,28 +14,6 @@ from itertools import permutations
 # Hamiltonian path or not. Print out every millionth path, as you go,
 # to show progress. Your output has to look substantially similar to mine,
 # as shown here for the start of running it for a 4x4 board:
-
-def to_chess(m, r=8, c=8):
-    """Return algebraic chess notation for square m in an r x chess board.
-    >>> to_chess(0), to_chess(1), to_chess(7), to_chess(8), to_chess(63)
-    ('a8', 'b8', 'h8', 'a7', 'h1')
-    """
-    row = m % r + 97
-    col = c - m // c
-    return chr(row) + str(col)
-
-
-def is_knight_move(j, k, c):
-    """ returns true if knight moves from (x0, y0) to (x1, y1),
-    where (|y1-y0|, |x1-x0|) ∈ {(1, 2), (2, 1)}
-    at most, there are 8 valid L-shaped moves, board edge permitting
-    >>> is_knight_move(51, 61, 8), is_knight_move(19, 4, 8), is_knight_move(0, 2, 8)
-    (True, True, False)
-    """
-    diff_col = abs((k % c) - (j % c)) - 1
-    diff_row = abs((k // c) - (j // c)) - 1
-    return (diff_col*diff_row == 0) and (diff_col + diff_row == 1)
-
 
 if __name__ == '__main__':
 
--- Chessboard.py
+++ Chessboard.py
@@ -1,10 +1,20 @@
 def to_chess(m, r=8, c=8):
     """Return algebraic chess notation for square m in an r x chess board.
-    >>> to_chess(0, 3), to_chess(1, 3), to_chess(7, 3), to_chess(8, 3)
-    ('a3', 'b3', 'b1', 'c1')
-    >>> to_chess(0), to_chess(1), to_chess(7), to_chess(8)
-    ('a8', 'b8', 'h8', 'a7')
+    >>> to_chess(0), to_chess(1), to_chess(7), to_chess(8), to_chess(63)
+    ('a8', 'b8', 'h8', 'a7', 'h1')
     """
     row = m % r + 97
     col = c - m // c
     return chr(row) + str(col)
+
+
+def is_knight_move(j, k, c):
+    """ returns true if knight moves from (x0, y0) to (x1, y1),
+    where (|y1-y0|, |x1-x0|) ∈ {(1, 2), (2, 1)}
+    at most, there are 8 valid L-shaped moves, board edge permitting
+    >>> is_knight_move(51, 61, 8), is_knight_move(19, 4, 8), is_knight_move(0, 2, 8)
+    (True, True, False)
+    """
+    diff_col = abs((k % c) - (j % c)) - 1
+    diff_row = abs((k // c) - (j // c)) - 1
+    return (diff_col*diff_row == 0) and (diff_col + diff_row == 1)
