34775758db97f5ec84a636d7c1fac193c0ef3277
markplotlib <markplotlib@gmail.com>
2020-03-20 10:46:27 -0700

    clear brute force lines, ready for backtracking changes

---
+++ BackTrack.py
@@ -0,0 +1,39 @@
+from itertools import permutations, repeat
+from time import time
+
+from Chessboard import to_chess, is_knight_move, are_all_knight_moves
+# https://seattleu.instructure.com/courses/1588778/assignments/6759181
+
+"""Backtracking algorithm
+DFS approach, random neighbor ordering
+recursive calls for promising branches
+"""
+# global variables
+row = col = 4   # board dimensions
+
+if __name__ == '__main__':
+
+    # generate iterable of all permutations
+    iter_permutations = permutations(range(row*col))
+
+    table_header = ["seconds", "iteration", "trying...", "{}x{} board moves".format(row, col)]
+    print("\t| ".join(table_header))
+
+    # convert each from tuple to string
+    start = time()
+    i = 0
+    for permutation_tuple in iter_permutations:
+        i += 1
+        if i % 1000000 == 0:
+            itr_time = round(time() - start, 1)
+
+            # table column: trying (attempt)
+
+            # table column: outcome
+            tour_done = are_all_knight_moves(permutation_tuple, col)
+            outcome = "success" if tour_done else "failure"
+
+            move_seq = list(map(to_chess, permutation_tuple, repeat(row, row*col), repeat(col, row*col)))
+
+            table_row = [str(itr_time), str(i), outcome, " ".join(move_seq)]
+            print("\t| ".join(table_row))
